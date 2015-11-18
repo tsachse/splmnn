64d91e578c91f84bf824dbb466db58aadee5e87f
sachse_t <thomas.sachse@web.de>
2015-11-18 15:05:34 +0100

    Es sieht schlecht aus

--- public/spielmann.js
+++ public/spielmann.js
@@ -27,13 +27,37 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
      .listActions(['show','edit']) 
      .actions(['create', 'delete'])  ;
 
-    media.showView().fields([
+     var tmpl='<div class="row">' +
+	 '   <div class="col-lg-12">' +
+	 '	<div class="page-header">' +
+	 '	    <ma-view-actions override="::showController.actions" entry="entry" entity="::showController.entity">' +
+	 '		<ma-list-button ng-if="::entity.listView().enabled" entity="::entity"></ma-list-button>' +
+	 '		<ma-edit-button ng-if="::entity.editionView().enabled" entry="entry" entity="::entity"></ma-edit-button>' +
+	 '		<ma-delete-button ng-if="::entity.deletionView().enabled" entry="entry" entity="::entity"></ma-delete-button>' +
+	 '	    </ma-view-actions>' +
+	 '	    <h1 compile="::showController.title">' +
+	 '		{{ ::showController.view.entity.name() | humanize:true | singularize }}  #{{ ::entry.identifierValue }} Detail' +
+	 '	    </h1>' +
+	 '	    <p class="lead" ng-if="::showController.description" compile="::showController.description">{{ ::showController.description }}</p>' +
+	 '	</div>' +
+	 '   </div>' +
+	 '  </div>' +
+	 ' <div><video id="qt_repeat" height=400 controls src="/media/BL-207.m4v" /></div>' +
+	 ' <div class="row form-horizontal" id="show-view" > ' +
+	  '    <div class="col-lg-12 form-group" ng-repeat="field in ::showController.fields track by $index">' +
+	'	  <label class="col-sm-2 control-label">{{ field.label() }}</label>' +
+	'	  <div class="show-value" ng-class="::"ng-admin-field-" + field.name() + " " + "ng-admin-type-" + field.type() + " " + (field.getCssClasses(entry) || "col-sm-10 col-md-8 col-lg-7")">' +
+	'	      <ma-column field="::field" entry="::entry" entity="::showController.entity" datastore="::showController.dataStore"></ma-column>' +
+	'	  </div>' +
+	'      </div>' +
+	'  </div>';
+    media.showView().template(tmpl).fields([
       nga.field('code'),
       nga.field('name'),
       nga.field('path'),
-      nga.field('v', 'template')
-        .label('')
-	.template('<video id="qt_repeat" height=400 controls src="{{ entry.values.path}}" />'),
+      //nga.field('v', 'template')
+      //  .label('')
+	//.template('<video id="qt_repeat" height=400 controls src="{{ entry.values.path}}" />'),
       nga.field('sections', 'referenced_list')
         .targetEntity(sections)
 	.targetReferenceField('medium_id')
@@ -43,7 +67,7 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
 	  nga.field('stop'),
 	  nga.field('b', 'template')
 	    .label('')
-	    .template('<button type="button" ng-click="ab_repeat(entry.values.start, entry.values.stop)" ng-controller="VideoCtrl"  class="btn btn-default btn-xs"><span class="glyphicon glyphicon-repeat"></span>&nbsp;</button>')
+	    .template('<button type="button" ng-click="ab_repeat(entry.values.start, entry.values.stop)" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-repeat"></span>&nbsp;</button>')
 
 
 	])
@@ -123,20 +147,23 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
 
 myApp.controller('VideoCtrl', function ($scope) {
   $scope.vid = document.getElementById("qt_repeat");
-  $scope.start = 0;
-  $scope.stop = -1;
+  //$scope.start = 0;
+  //$scope.stop = -1;
 
+  /*
   $scope.ab_repeat = function(start, stop) {
+    $scope.start  = start;
+    $scope.stop = stop;
+    $scope.vid.currentTime = $scope.start;
+    console.log($scope.vid.currentTime);
     $scope.vid.play();
-    //$scope.start  = start;
-    //$scope.stop = stop;
-    //$scope.vid.currentTime = $scope.start;
     $scope.vid.addEventListener('timeupdate',function() {
       var v = document.getElementById("qt_repeat");
+      if($scope.vid.currentTime > $scope.stop) {
       console.log('......', v.currentTime );
-      //if($scope.stop > -1 && $scope.vid.currentTime > $scope.stop) {
-//$scope.vid.currentTime = $scope.start;
-//      }
+          $scope.vid.currentTime = $scope.start;
+      }
     });
   };
+  */
 });
